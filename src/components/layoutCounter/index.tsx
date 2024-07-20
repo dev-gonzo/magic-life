15c15d464c3339623eefdeae21d39187c5a57100
Victor Chr <victotasassin@gmail.com>
2024-07-20 02:36:14 -0300

    feat: Criação da pasta fivePlayers.

--- src/components/layoutCounter/index.tsx
+++ src/components/layoutCounter/index.tsx
@@ -1,8 +1,10 @@
 import { RollDice } from "../../@types";
 import { useGamePlayers } from "../../storeds/useThemeMode/useGamePlayers";
-import { FourPlayers } from "./fourPlayers";
-import { SixPlayers } from "./sixPlayers";
 import { TwoPlayers } from "./twoPlayers";
+import { ThreePlayers } from "./threePlayers";
+import { FourPlayers } from "./fourPlayers";
+import { FivePlayers } from "./fivePlayers";
+import { SixPlayers } from "./sixPlayers";
 
 export const LayoutCounter = ({ rollDice }: RollDice) => {
   const { players } = useGamePlayers();
@@ -11,9 +13,9 @@ export const LayoutCounter = ({ rollDice }: RollDice) => {
     <></>, //player 0 = 0
     <></>, //player 1 = 1
     <TwoPlayers key={1} rollDice={rollDice} />,
-    <></>, //player 3 = 3
+    <ThreePlayers key={2} rollDice={rollDice}/>,
     <FourPlayers key={3} rollDice={rollDice} />,
-    <></>,
+    <FivePlayers key={4} rollDice={rollDice} />,
     <SixPlayers key={5} rollDice={rollDice} />,
   ];
   return <>{layout[players?.length]}</>;
