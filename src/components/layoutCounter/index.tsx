98eb302135b9e2b081ba97824e12a1ea9543465f
Victor Chr <victotasassin@gmail.com>
2024-07-20 00:22:19 -0300

    criação do layot 6

--- src/components/layoutCounter/index.tsx
+++ src/components/layoutCounter/index.tsx
@@ -1,6 +1,7 @@
 import { RollDice } from "../../@types";
 import { useGamePlayers } from "../../storeds/useThemeMode/useGamePlayers";
 import { FourPlayers } from "./fourPlayers";
+import { SixPlayers } from "./sixPlayers";
 
 export const LayoutCounter = ({ rollDice }: RollDice) => {
   const { players } = useGamePlayers();
@@ -11,8 +12,9 @@ export const LayoutCounter = ({ rollDice }: RollDice) => {
     <></>,
     <></>,
     <FourPlayers key={3} rollDice={rollDice} />,
-    <></>,
+    <SixPlayers key={5} rollDice={rollDice} />,
     <></>,
   ];
-  return <>{layout[players?.length]}</>;
+  //return <>{layout[players?.length]}</>;
+  return <SixPlayers key={4} rollDice={rollDice} />
 };
