3b1e3dce5ea46b2e317767da9b0bf2ec7cc0718d
Eduardo Di Lauro <elauro@magnasistemas.com.br>
2024-07-19 23:53:36 -0300

    fix ajuste build

--- src/components/layoutCounter/index.tsx
+++ src/components/layoutCounter/index.tsx
@@ -1,25 +1,18 @@
 import { RollDice } from "../../@types";
-import { getRegistredQtd } from "../../helpers/registredPlayers";
-import { EightPlayers } from "./eightPlayers";
-import { FivePlayers } from "./fivePlayers";
+import { useGamePlayers } from "../../storeds/useThemeMode/useGamePlayers";
 import { FourPlayers } from "./fourPlayers";
-import { SevenPlayers } from "./sevenPlayers";
-import { SixPlayers } from "./sixPlayers";
-import { ThreePlayers } from "./threePlayers";
-import { TwoPlayers } from "./twoPlayers";
 
 export const LayoutCounter = ({ rollDice }: RollDice) => {
-  const players = getRegistredQtd();
+  const { players } = useGamePlayers();
 
   const layout = [
     <></>,
-    <TwoPlayers key={1} />,
-    <ThreePlayers key={2} />,
+    <></>,
+    <></>,
+    <></>,
     <FourPlayers key={3} rollDice={rollDice} />,
-    <FivePlayers key={4} />,
-    <SixPlayers key={5} />,
-    <SevenPlayers key={6} />,
-    <EightPlayers key={7} />,
+    <></>,
+    <></>,
   ];
-  return <>{layout[players]}</>;
+  return <>{layout[players?.length]}</>;
 };
